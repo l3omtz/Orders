95b878f5cb263c995f9b1f24ddea069e3d6de07a
Leo Martinez <l3o@Leos-MacBook-Pro.local>
2017-02-27 20:34:36 -0600

    updated connection server

--- models/database.js
+++ models/database.js
@@ -35,3 +35,13 @@ pool.connect (function(err, client, done) {
   });
 
 }); // END
+
+pool.on('error', function(err, client) {
+  // if an error is encountered by a client while it sits idle in the pool
+  // the pool itself will emit an error event with both the error and
+  // the client which emitted the original error
+  // this is a rare occurrence but can happen if there is a network partition
+  // between your application and the database, the database restarts, etc.
+  // and so you might want to handle it and at least log it out 
+  console.error('idel client error', err.message, err.stack);
+});
